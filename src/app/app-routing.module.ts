ecf84c29bb497a0211211459a9755f38339280a7
Ponce <aldrinponce@gmail.com>
2019-08-10 14:07:48 +0800

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddformComponent } from './addform/addform.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add-form', component: AddformComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
