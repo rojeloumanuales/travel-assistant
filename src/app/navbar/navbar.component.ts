5f58879c971d63ad3f7aaa2766742b5057669966
LAPTOP-L8ME9RSQ\rojel <rojeloumanuales@gmail.com>
2019-08-10 17:25:20 +0800

    Push updates

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,9 +8,16 @@ import { UsersService } from '../services/users.service';
 })
 export class NavbarComponent implements OnInit {
 
+    isLoggedIn: boolean;
+
     constructor(private uService: UsersService) { }
 
     ngOnInit() {
+        if (this.uService.isLoggedIn()) {
+            this.isLoggedIn = true;
+        } else {
+            this.isLoggedIn = false;
+        }
     }
 
     logout() {
