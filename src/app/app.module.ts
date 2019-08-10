e2877a26f7579d3f982207ade78566781fc0e489
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-08-10 15:15:00 +0800

    Updated dashboard and app module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms'; 
 // Custom Import
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
+import { MatCheckboxModule} from '@angular/material/checkbox';
 import { AddformComponent } from './addform/addform.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './navbar/navbar.component';
