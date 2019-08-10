b8617dfc13e1303a38943590e3b379cfd91bf289
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-08-10 14:09:58 +0800

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashBoardComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
     BrowserModule,
     AppRoutingModule
   ],
